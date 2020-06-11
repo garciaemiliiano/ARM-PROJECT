fe2921a56182b15c7f37b157ba705fd497d32aef
Emiliano Garcia <55071268+garciaemiliiano@users.noreply.github.com>
2020-06-11 19:20:18 -0300

    firstCommit

---
+++ input-output.s
@@ -0,0 +1,24 @@
+.data 
+
+mensaje: .ascii ""
+.text 
+
+.global main
+
+main: 
+    mov r7, #3
+    mov r0, #0
+    mov r2, #10
+    ldr r1, =mensaje
+    swi 0
+
+_escribir:
+    mov r7, #4
+    mov r0, #1
+    mov r2, #6 /*Numero de letras */
+    ldr r1, =mensaje
+    swi 0
+
+fin: 
+    mov r7, #1 
+    swi 0
