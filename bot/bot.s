a8b0419aeb09f24702703d2a1b613d111a51e13e
tomasArtaza <tomasgun_10@outlook.com>
2020-06-13 17:01:05 -0300

    Ya Muestra Numeros

--- bot/bot.s
+++ bot/bot.s
@@ -1,25 +1,27 @@
 .data 
-
-mensaje: .ascii ""
+mensajePorPantalla: .ascii "Ingrese una palabra de 4 digitos, porFavor \n"
+cadenaVacia: .word 0
 .text 
-
-
 .global main
-
 main: 
+    mov r7, #4      /* mostrar por standar output*/
+    mov r0, #1      /*ingreso string*/
+    mov r2, #100    /*Tamaño string RANDOM*/
+    ldr r1, =mensajePorPantalla
+    swi 0
+mostrarEnPantalla: 
     mov r7, #3
     mov r0, #0
     mov r2, #10
-    ldr r1, =mensaje
+    ldr r1, =cadenaVacia
     swi 0
-/*hola */
-_escribir:
+
+/*INPUT */
     mov r7, #4
     mov r0, #1
-    mov r2, #6 /*Numero de letras */
-    ldr r1, =mensaje
-    swi 0 /*hola */
-
-fin: 
-    mov r7, #1 /*AA */
+    mov r2, #100 /*Numero de letras para escribir*/
+    ldr r1, =cadenaVacia
+    swi 0
+fin:
+    mov r7, #1
     swi 0
