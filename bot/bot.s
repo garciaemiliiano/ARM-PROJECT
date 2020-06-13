f2da5a368aadc90642d7eb295707a26c433198f2
tomasArtaza <tomasgun_10@outlook.com>
2020-06-13 15:57:25 -0300

    commit bot

--- bot/bot.s
+++ bot/bot.s
@@ -3,6 +3,7 @@
 mensaje: .ascii ""
 .text 
 
+
 .global main
 
 main: 
@@ -17,7 +18,7 @@ _escribir:
     mov r0, #1
     mov r2, #6 /*Numero de letras */
     ldr r1, =mensaje
-    swi 0
+    swi 0 /*hola */
 
 fin: 
     mov r7, #1 /*AA */
